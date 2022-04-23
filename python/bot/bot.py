b3d281d632046b07915d06dd201c4b2b979aa148
ZhanQuan <76029680+ZhanQuan123@users.noreply.github.com>
2022-04-23 09:46:35 +0800

    Update bot.py

--- python/bot/bot.py
+++ python/bot/bot.py
@@ -15,13 +15,10 @@ def start():
     @bot.event
     async def on_ready( ):
         print(bot.user.name + " has started.")
-        os.chdir('../')
-
-
-        # dir_path = os.path.dirname(os.path.realpath(__file__))
-        # for file in os.listdir(f"{dir_path}/cogs/"): # lists all the cog files inside the cog folder.
-        #     if file.endswith(".py"): # It gets all the cogs that ends with a ".py".
-        #         name = file[:-3] # It gets the name of the file removing the ".py"
-        #         await bot.load_extension(f"commands.{name}") # This loads the cog.
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        for file in os.listdir(f"{dir_path}/cogs/"): # lists all the cog files inside the cog folder.
+            if file.endswith(".py"): # It gets all the cogs that ends with a ".py".
+            name = file[:-3] # It gets the name of the file removing the ".py"
+            await bot.load_extension(f"commands.{name}") # This loads the cog.
 
     bot.run(config.token())
